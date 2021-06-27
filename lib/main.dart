5ad8257a39367396631ce5744e4184803c264cac
Nilesh Shiragave <snilesh.com@gmail.com>
2021-06-26 19:41:40 -0700

    Day 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/homepage.dart';
+import 'homepage.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to 30 Days of Flutter'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
